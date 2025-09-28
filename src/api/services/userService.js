ac357d43591e77bae828f279c898082522188a03
Владимир Третьяков <Moonfox923@gmail.com>
2025-09-28 14:12:00 +0700

    Сделал usersService

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,18 +1,49 @@
 // Сервис для работы с пользователями
 
-const mockUsers = [];
+import { API_MODE, CURRENT_MODE } from "../config";
 
+// Данные для теста
+const mockUsers = [
+	{
+		id: 0,
+		email: 'example@gmail.com',
+		password: 'password',
+		first_name: 'first name',
+		name: 'name',
+		patronymic: '',
+		birth_date: '',
+		sex: '',
+		country: '',
+		region: '',
+		place: '',
+		photo: '',
+		resume: '',
+		energy: 0,
+		expirience: 0,
+		rank_id: 0,
+		direction: '',
+		role: ''
+	}
+];
+
+// Сервис с запросами на rest api
 const realUserService = {
 
 };
 
+// Сервис для теста
 const mockUserService = {
 	getUsers: async () => {
 		return mockUsers;
 	},
+
 	getUserById: async (id) => {
 		const user = mockUsers.find(u => u.id === id);
 		if (!user) throw new Error('Пользователь не найден');
 		return user;
-	}
+	},
 };
+
+export const userService = CURRENT_MODE === API_MODE.MOCK
+	? mockUserService
+	: realUserService;
