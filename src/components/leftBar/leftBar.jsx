8b588ff6132ecee6fb6f6094f63d70969e4e422d
StarCookie256 <sopov.cookie@gmail.com>
2025-09-28 16:24:31 +0700

    алабугага страничка с артефактами, почти done (+иконочка)

--- src/components/leftBar/leftBar.jsx
+++ src/components/leftBar/leftBar.jsx
@@ -6,7 +6,8 @@ export default function LeftBar(){
 
     const barPages = [
         {path: '/cabinet', icon: '/alabuga_icon_cabinet.png'},
-        {path: '/shop', icon: '/alabuga_icon_shop.png'}
+        {path: '/shop', icon: '/alabuga_icon_shop.png'},
+        {path: '/artefacts', icon:'/alabuga_icon_artefactPage.png'}
     ]
 
     return(
